a95ab2d3db50c6f80470171c40b3111f2380012c
I306454 <jiabin.zheng01@sap.com>
2021-12-01 21:03:44 +0800

    Finish d4 practice

--- crud-node/app.js
+++ crud-node/app.js
@@ -5,12 +5,19 @@
 var express = require('express')
 var router = require('./router')
 var app = express()
+var bodyParser = require('body-parser')
 
 
 app.use('/node_modules/', express.static('./node_modules'))
 app.use('/public/', express.static('./public'))
 
 app.engine('html', require('express-art-template'))
+
+// 必须在挂在 router 之前调用
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(router)
 
--- crud-node/router.js
+++ crud-node/router.js
@@ -1,31 +1,72 @@
 var fs = require('fs')
+var Student = require('./student')
 var express = require('express')
 var router = express.Router()
 
 router.get('/students', function (req, res) {
-    fs.readFile('./db.json', 'utf8', function(err, data) {
+    Student.find(function(err, students) {
         if( err ) {
             return res.status(500).send('Server error.')
         }
-
-        var context = JSON.parse(data).students
-
         res.render('index.html', {
             fruits: [
                 "apple",
                 "banana",
                 "orange"
             ],
-            students: context
+            students: students
         })
     })
 })
 
-router.get('/students/new', function(req, res) {})
-router.post('/students/new', function(req, res) {})
-router.get('/students/edit', function(req, res) {})
-router.post('/students/edit', function(req, res) {})
-router.get('/students/delete', function(req, res) {})
+router.get('/students/new', function(req, res) {
+    res.render('new.html')
+})
+router.post('/students/new', function(req, res) {
+    Student.save(req.body, function(err) {
+        if (err) {
+            return res.status(500).send('Server error.')
+        }
+        res.redirect('/students')
+    })
+})
+router.get('/students/edit', function(req, res) {
+    // 1. index 列表加入 edit 链接
+    // 2. 获取学生 id
+    // 3. 渲染编辑页面
+    Student.findById(parseInt(req.query.id), function(err, student) {
+        if (err) {
+            return res.status(500).send('Server error.')
+        }
+        res.render('edit.html', {
+            student: student
+        })
+    })
+})
+router.post('/students/edit', function(req, res) {
+    // 获取表单数据
+    // 更新
+    // 发送响应
+    Student.updateById(req.body, function(err) {
+        if (err) {
+            return res.status(500).send('Server error.')
+        }
+
+        res.redirect('/students')
+    })
+})
+router.get('/students/delete', function(req, res) {
+    // 获取 id
+    // 根据 id 删除
+    // 相应
+    Student.deleteById(req.query.id, function(err) {
+        if (err) {
+            return res.status(500).send('Server error.')
+        }
+
+        res.redirect('/students')
+    })
+})
 
 
 module.exports = router
